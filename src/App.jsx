d63ca9f8c034b7fa19e78c6f693da1c930fb51a1
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-06-10 12:03:35 +0600

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-6xl text-center my-10 font-bold'>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/Route.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import App from '../App';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>,
+  },
+]);
+
+export default router;
